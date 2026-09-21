f42703f609b7ccaec26beeec12dd4cbf552cd248
Mayuresh Palmate <mayureshpalmate.mp@gmail.com>
2026-09-21 21:12:09 +0530

    Enhance website scanner with additional security checks
    Added security scan checks for X-Frame-Options and Content-Security-Policy headers.

--- scanner.py
+++ scanner.py
@@ -3,13 +3,22 @@
 import requests
 url = input('Enter the URL of the website: ')
 response = requests.get(url)
+print("\n--- Security Scan ---")
+print("Target:", url)
 #Python, send a GET request to the website stored in url
-print(response.status_code)
-#if the code comes as 200
-print(response.headers) 
+print("Status Code:", response.status_code)
+#if the code comes as 200 it means its executed succsessfully
 if"Strict-transport-security" in response.headers:
     print('HSTS is present')
 else:
-    print('_________HSTS is not present _________')
+    print('HSTS is not present')
 # we ask python if this header exists in the response header if yes print present else print not present
-
+if "X-Frame-Options" in response.headers:
+    print("X-Frame-Options: Present")
+else:
+    print("X-Frame-Options: NOT Present")
+# WE again ask python if the above header is there
+if "Content-Security-Policy" in response.headers:
+    print("Content-Security-Policy: Present")
+else:
+    print("Content-Security-Policy: NOT Present")
